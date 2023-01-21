4bfd4bd592547065d673aeee916af3b76cc00be9
Дмитрий Кассин <kassindima1@gmail.com>
2023-01-21 07:30:45 +0200

    //

--- Proj/Proj/Controllers/WeatherForecastController.cs
+++ Proj/Proj/Controllers/WeatherForecastController.cs
@@ -1,6 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 
-namespace Project1.Controllers
+namespace Proj.Controllers
 {
     [ApiController]
     [Route("[controller]")]
--- Proj/Proj/Program.cs
+++ Proj/Proj/Program.cs
@@ -1,6 +1,3 @@
-using Microsoft.EntityFrameworkCore;
-using Project1.Models;
-
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -15,17 +12,6 @@ if (!app.Environment.IsDevelopment())
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
-builder.Services.AddControllers();
-builder.Services.AddCors(p => p.AddPolicy("corsapp", builder =>
-{
-    builder.WithOrigins("*").AllowAnyMethod().AllowAnyHeader();
-}));
-builder.Services.AddDbContext<ProductContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("ProductConnection")));
-
-//var app = builder.Build();
-app.UseCors("corsapp");
-app.UseAuthorization();
-app.MapControllers();
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
