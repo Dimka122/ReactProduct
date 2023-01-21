a6d3015c09359a795741f38028dbf11869477d32
Дмитрий Кассин <kassindima1@gmail.com>
2023-01-21 08:37:54 +0200

    111

---
+++ Proj/Proj/Controllers/ProductController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Proj.Models;
+
+namespace Proj.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        private readonly ProductContext _productContext;
+        public ProductController(ProductContext productContext)
+        {
+            _productContext = productContext;
+        }
+        [HttpPost]
+        public async Task<ActionResult<Product>> AddMovie(Product product)
+        {
+            if (_productContext == null)
+            {
+                return NotFound();
+            }
+
+            _productContext.ProductList.Add(product);
+
+            await _productContext.SaveChangesAsync();
+
+            return product;
+        }
+    }
+}
